4bb8cc328b0fa61bb27afabb26d5f1d1636a0433
Zacharie Tevaearai <zacharietevaearai@epfl.ch>
2021-10-05 20:32:33 +0200

    Fix typo.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@
 //!
 //! `Cheap8` is a simple Chip8 emulator, bundled with
 //! mutliple clients, like a sdl based, terminal based
-//! adn hopefully wasm one.
+//! and hopefully a wasm one.
 pub use cheap8_core;
 
